75ff53c4aa2ab64454e68b9c53c20c1b9590653a
sebastianer <sebastian@arteco-consulting.com>
2024-06-26 10:23:24 +0200

    Aplicación base

---
+++ src/main/java/com/example/demo/GreetingsController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api")
+public class GreetingsController {
+
+    @GetMapping(value = "/{name}", produces = "application/json")
+    public String getGreeting(@PathVariable String name){
+        return "Saludos " + name + "!";
+    }
+
+}
